2bda0cc541de70afa38fd4e3473cf51d163fd2b4
ushasri reddy <vemuruushasri@gmail.com>
2026-02-11 09:51:32 +0530

    Time: 6 ms (84.24%), Space: 47.2 MB (35.81%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstUniqChar(String s) {
+
+        int[] count = new int[26];
+
+        // Step 1: Count frequency of each character
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        // Step 2: Find first character with frequency 1
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
