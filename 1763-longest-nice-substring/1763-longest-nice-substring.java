72a7e7d2ccb9a4f908ee1771ff0e1a7529b73883
ushasri reddy <vemuruushasri@gmail.com>
2026-05-08 00:16:02 +0530

    Time: 1 ms (98.56%), Space: 42.9 MB (89.16%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        if (s.length() < 2) {
+            return "";
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (s.indexOf(Character.toLowerCase(ch)) == -1 ||
+                s.indexOf(Character.toUpperCase(ch)) == -1) {
+
+                String left = longestNiceSubstring(s.substring(0, i));
+                String right = longestNiceSubstring(s.substring(i + 1));
+
+                return left.length() >= right.length() ? left : right;
+            }
+        }
+
+        return s;
+    }
+}
